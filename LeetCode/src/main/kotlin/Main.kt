7f25061a424cb1efee233006a3849c659c83845e
Tolga Pirim <tolgapirim25@gmail.com>
2022-06-08 16:19:17 +0300

    solved palindrome

--- LeetCode/src/main/kotlin/Main.kt
+++ LeetCode/src/main/kotlin/Main.kt
@@ -5,4 +5,7 @@ fun main() {
     var result = solution.twoSum(data.twoSumArray(), 9)
 
     println("${result!![0]} ${result[1]}")
+
+    println(solution.isPalindrome(121))
+
 }
--- LeetCode/src/main/kotlin/Solution.kt
+++ LeetCode/src/main/kotlin/Solution.kt
@@ -17,4 +17,23 @@ class Solution {
         return null
     }
 
+    // 121 is palindrom , 214 is not palindrom
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) {
+            return false
+        }
+
+        var num = x
+        var reversed = 0
+
+        while (num != 0) {
+            val digit = num % 10
+            reversed = reversed * 10 + digit
+            num /= 10
+        }
+
+
+        return reversed == x
+    }
+
 }
